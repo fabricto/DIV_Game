c5df1a37f725e5ddde35d32de600e5c91a5dbcea
Fabricio <43271898+fabricto@users.noreply.github.com>
2020-02-13 14:05:15 +0100

    assets
    inicio

---
+++ GDD.md
@@ -0,0 +1,34 @@
+
+CONCEPTO    
+    Título: 
+    Diseñadores:
+        Fabricio Torres Samueza
+        Sergio Mellado
+    Plataforma: Web
+    Versión: 1 12/02/2020
+    Sinopsis de Jugabilidad y Contenido:
+    Licencia:
+    Publico:
+HISTORIAL DE VERSIONES
+
+VISIÓN GENERAL DEL JUEGO
+
+MECÁNICA DEL JUEGO
+
+NIVELES
+
+PROOGRESO DEL JUEGO
+
+PERSONAJES
+
+ENEMIGOS
+
+HABILIDADES
+
+ARMAS
+
+ITEMS
+
+MIEMBROS DEL EQUIPO
+
+DETALLES DE PRODUCCIÓN
--- game.js
+++ game.js
@@ -3,11 +3,21 @@ export default class Game extends Phaser.Scene {
     super({ key: 'main' });
   }
   preload() {  
+    this.load.image("dino", "/assets/dino/walk1.png");
+    this.load.image("mundo2", "/assets/mundo2/BG.png")
+    
   }
 
   create() {
+    let posy =200; 
+    let posx= this.sys.game.config.height-100;
+    let center_width = this.sys.game.config.width/2;
+    let center_height = this.sys.game.config.height/2;
+   //this.dino = this.add.image(posy,posx,"dino").setDisplaySize(200,140);
+   this.mundo2 = this.add.image(center_width,center_height,"mundo2");  
   }
 
   update(time, delta) {    
+   // this.dino.x++;
   }
 }
